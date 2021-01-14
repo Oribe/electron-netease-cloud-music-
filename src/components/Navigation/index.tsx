bcfead4bd08a7b6be0353b83a34ccb3526d42d2d
Oribe <d_egoist@163.com>
2021-01-14 22:13:30 +0800

    前进后退

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -5,9 +5,9 @@ import styles from "./index.module.scss";
 const Navigation: FC = () => {
   return (
     <div className={styles.navgator}>
-      <div>
-        <span>{"<"}</span>
-        <span>{">"}</span>
+      <div className={styles.arrowContainer}>
+        <div className={styles.arrowLeft}></div>
+        <div className={styles.arrowRight}></div>
       </div>
     </div>
   );
